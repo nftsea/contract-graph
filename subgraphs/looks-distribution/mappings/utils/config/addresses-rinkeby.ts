5e5dfbbafb55fc16a9136bf12ac629e10f3ed933
Jackie Yan <zhejyan@microsoft.com>
2022-06-26 18:33:04 +0800

    first commit

---
+++ subgraphs/looks-distribution/mappings/utils/config/addresses-rinkeby.ts
@@ -0,0 +1,4 @@
+import { Address } from "@graphprotocol/graph-ts";
+
+export const AGGREGATOR_ADDRESS = Address.fromString("0x5b5a702939bf53595dd0c14488528e05e1e8c4a5");
+export const FEE_SHARING_ADDRESS = Address.fromString("0xf32e6141d54512814fb94584fc17baaf0c1203de");
